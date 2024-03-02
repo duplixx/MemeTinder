59a31acdea7d1d4d849b25db4b2abc7f9e631c82
duplixx <90516956+duplixx@users.noreply.github.com>
2024-03-02 20:29:36 +0530

    Initial Commit

---
+++ MemeTinder/MemeTinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemeTinderApp.swift
+//  MemeTinder
+//
+//  Created by Shekhar Patel on 02/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MemeTinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
